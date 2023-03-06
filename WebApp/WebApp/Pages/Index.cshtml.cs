08c2ba38dd7ec555f165f6fd3065c395b43bba87
mesri <abde_mes@yahoo.fr>
2023-03-06 19:11:42 +0100

    Action Injection insead of constructor injection

--- WebApp/WebApp/Pages/Index.cshtml.cs
+++ WebApp/WebApp/Pages/Index.cshtml.cs
@@ -6,17 +6,11 @@ using WebApp.services;
 namespace WebApp.Pages
 {
     public class IndexModel : PageModel
-    {
-        private readonly ILog _log;
+    {       
 
-        public IndexModel(ILog log)
+        public void OnGet([FromServices] ILog log)
         {
-            _log = log;
-        }
-
-        public void OnGet()
-        {
-            _log.info("Indexe page requested !");
+            log.info("Indexe page requested !");
         }
     }
 }
